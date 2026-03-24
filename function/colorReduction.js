4cc5742d83d3221e7426f91a59de39ac86eb2282
ゆずれもん <135597701+rumashili@users.noreply.github.com>
2026-03-24 11:33:07 +0900

    ud

--- function/colorReduction.js
+++ function/colorReduction.js
@@ -4,9 +4,9 @@ export function reduceColor(data, level) {
   const step = 2 ** level;
 
   for (let i = 0; i < data.length; i += 4) {
-    data[i]   = Math.floor(data[i]   / step) * step;
-    data[i+1] = Math.floor(data[i+1] / step) * step;
-    data[i+2] = Math.floor(data[i+2] / step) * step;
+    data[i]   = Math.round(data[i]   / step) * step;
+    data[i+1] = Math.round(data[i+1] / step) * step;
+    data[i+2] = Math.round(data[i+2] / step) * step;
   }
 
   return data;
